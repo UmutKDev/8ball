b4b854294a03da4b34d6a4b56a4df4409b2968da
Umut Kızıloğlu <umutkiziloglu00@gmail.com>
2022-05-03 15:05:12 +0000

    .

--- pages/index.jsx
+++ pages/index.jsx
@@ -58,15 +58,15 @@ const Index = () => {
 
   return (
     <div className='h-screen w-screen flex justify-center items-center'>
-      <div className={`contianer absolute max-w-8/12 w-8/12 max-h-5/6 h-5/6 border-2 rounded flex items-center justify-center flex-col flex-wrap ${answer ? "-space-y-3" : "space-y-6"}`}>
+      <div className={`contianer absolute max-w-8/12 w-8/12 max-h-5/6 h-5/6 border-2 border-gray-200 border-opacity-30 rounded flex items-center justify-center flex-col flex-wrap ${answer ? "space-y-6" : "space-y-10"}`}>
         {answer ?
           <>
             <div>
-              <h1 className='text-3xl'>İşte Cevabın...</h1>
+              <h1 className='text-3xl text-white'>İşte Cevabın...</h1>
             </div>
             <div className='relative flex justify-center'>
-              <Image className='w-full max-w-[40em]' src='/magic_ball.png' width={640} height={518} />
-              <p className='absolute text-white top-[39%] left-[47%] text-xs w-[11%] text-[80%] uppercase text-center m-0'>{answer}</p>
+              <Image className='w-full max-w-[40em]' src='/magic_ball.png' width={550} height={434} />
+              <p className='absolute text-white top-[39%] left-[46%] text-xs w-[11%] text-[80%] uppercase text-center m-0'>{answer}</p>
             </div>
             <div>
               <button type="button" className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-base px-5 py-2.5 text-center mr-2 mb-2" onClick={erase}>Geri Dön!</button>
@@ -75,14 +75,14 @@ const Index = () => {
           :
           <>
             <div>
-              <h1 className='text-5xl'>Sihirli Top</h1>
+              <h1 className='text-5xl text-transparent bg-clip-text bg-gradient-to-r from-red-500 via-green-500 to-blue-500'>Sihirli Top</h1>
             </div>
             <div>
               <Image src='/8ball.png' width={200} height={200} draggable={false} />
             </div>
             <div className='flex justify-center w-1/2 flex-wrap flex-row space-y-6'>
-              <input id="message" value={userInput} onChange={handleChange} rows="3" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" placeholder="Leave a comment..." />
-              <button type="button" className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-base px-5 py-2.5 text-center mr-2 mb-2" onClick={handleClick}>Top'a Sor!</button>
+              <input value={userInput} onChange={handleChange} type="text" className="text-sm rounded-lg  block w-full p-2.5 bg-neutral-600 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder='İlerde evlenebilecekmiyim?' required />
+              <button type="button" className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-lg px-5 py-2.5 text-center mr-2 mb-2" onClick={handleClick}>Top'a Sor!</button>
             </div>
           </>
         }
