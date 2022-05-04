190dda89191798c89fa20362bfc56ae5ad13be39
Umut Kızıloğlu <umutkiziloglu00@gmail.com>
2022-05-04 10:13:19 +0000

    .

--- pages/index.jsx
+++ pages/index.jsx
@@ -44,7 +44,7 @@ const Index = ({ }) => {
               <h1 className='text-3xl text-white'>İşte Cevabın...</h1>
             </div>
             <div className='relative flex justify-center'>
-              <Image className='w-full max-w-[40em]' src='/answerside.png' width={550} height={434} />
+              <Image className='w-full max-w-[40em]' src='/answerside.png' width={550} height={434} alt="answer" />
               <p className='absolute text-white top-[36%] left-[46%] text-xs w-[11%] text-[80%] uppercase text-center m-0'>{answer}</p>
             </div>
             <div>
@@ -57,11 +57,11 @@ const Index = ({ }) => {
               <h1 className='text-5xl text-transparent bg-clip-text bg-gradient-to-r from-red-500 via-green-500 to-blue-500'>Sihirli Top</h1>
             </div>
             <div>
-              <Image src='/8ball.png' width={200} height={200} draggable={false} />
+              <Image src='/8ball.png' width={200} height={200} draggable={false} alt="8ball" />
             </div>
             <div className='flex justify-center w-1/2 flex-wrap flex-row space-y-6'>
               <input value={userInput} onChange={handleChange} type="text" className="text-sm rounded-lg  block w-full p-2.5 bg-neutral-600 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder='İlerde evlenebilecekmiyim?' required />
-              <button type="button" className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-lg px-5 py-2.5 text-center mb-2" onClick={handleClick}>Top'a Sor!</button>
+              <button type="button" className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-lg px-5 py-2.5 text-center mb-2" onClick={handleClick}>Topa Sor!</button>
             </div>
           </>
         }
