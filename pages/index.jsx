b86711bda298e90be38aeff7f948c53bb0f4dbfc
Umut Kızıloğlu <umutkiziloglu00@gmail.com>
2022-05-04 07:50:34 +0000

    .

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Image from 'next/image'
 
-const Index = () => {
+const Index = ({}) => {
 
   const [userInput, setUserInput] = useState('');
   const [randomIndex, setRandomIndex] = useState('');
@@ -31,6 +31,12 @@ const Index = () => {
     setError('');
   }
 
+  var answersS;
+
+  fetch(`https://jsonplaceholder.typicode.com/todos/1`).then(response => response.json()).then(data => {
+    answersS = data;
+    console.log(answersS);
+  });
 
   const possibleAnswers = [
     'It is certain',
@@ -65,11 +71,11 @@ const Index = () => {
               <h1 className='text-3xl text-white'>İşte Cevabın...</h1>
             </div>
             <div className='relative flex justify-center'>
-              <Image className='w-full max-w-[40em]' src='/magic_ball.png' width={550} height={434} />
+              <Image className='w-full max-w-[40em]' src='/answerside.png' width={550} height={434} />
               <p className='absolute text-white top-[39%] left-[46%] text-xs w-[11%] text-[80%] uppercase text-center m-0'>{answer}</p>
             </div>
             <div>
-              <button type="button" className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-base px-5 py-2.5 text-center mr-2 mb-2" onClick={erase}>Geri Dön!</button>
+              <button type="button" className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-base px-5 py-2.5 text-center mb-2" onClick={erase}>Geri Dön!</button>
             </div>
           </>
           :
@@ -82,7 +88,7 @@ const Index = () => {
             </div>
             <div className='flex justify-center w-1/2 flex-wrap flex-row space-y-6'>
               <input value={userInput} onChange={handleChange} type="text" className="text-sm rounded-lg  block w-full p-2.5 bg-neutral-600 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder='İlerde evlenebilecekmiyim?' required />
-              <button type="button" className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-lg px-5 py-2.5 text-center mr-2 mb-2" onClick={handleClick}>Top'a Sor!</button>
+              <button type="button" className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-lg px-5 py-2.5 text-center mb-2" onClick={handleClick}>Top'a Sor!</button>
             </div>
           </>
         }
@@ -93,14 +99,17 @@ const Index = () => {
 
 export default Index;
 
-// {/* <div>
-// <h1 className='text-5xl'>Sihirli Top</h1>
-// </div>
-// <div>
-// {/* <Image className='rounded-lg' src='/magic_ball.jpg' width={450} height={355} /> */}
-// <Image src='/8ball.png' width={200} height={200} draggable={false} />
-// </div >
-//   <div className='flex justify-center w-1/2 flex-wrap flex-row space-y-6'>
-//     <input id="message" value={userInput} onChange={handleChange} rows="3" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" placeholder="Leave a comment..." />
-//     <button type="button" className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-base px-5 py-2.5 text-center mr-2 mb-2" onClick={handleClick}>Top'a Sor!</button>
-//   </div> * /}
+// export async function getServerSideProps() {
+
+//   const response = await fetch(`https://raw-api.vercel.app/api/json/8ball?soru=${"sa"}&key=test`)
+
+//   const data = await response.json()
+
+
+//   console.log(data)
+//   return {
+//     props: {
+//       data
+//     }
+//   }
+// }
