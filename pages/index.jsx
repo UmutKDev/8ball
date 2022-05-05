3de507da8d9c59d1e8c91dcc08ea2023fc8ff7bd
Umut Kızıloğlu <umutkiziloglu00@gmail.com>
2022-05-05 13:04:22 +0000

    .

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import Image from 'next/image'
 
 const Index = ({ }) => {
 
@@ -61,7 +60,7 @@ const Index = ({ }) => {
         {
           answer &&
           <div className='w-8/12 h-4/6 flex items-center justify-center flex-col rounded-lg  p-3 bg-white  border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 '>
-            <button onClick={erase} type="button" class="mb-3 text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2">Tekrar Topa Sor!</button>
+            <button onClick={erase} type="button" className="mb-3 text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2">Tekrar Topa Sor!</button>
             <span className='text-gray-400 text-xs border-2 rounded p-1'><a className='text-red-400' href='https://www.umutk.codes'>Umut K.</a> & <a href="https://www.instagram.com/arrow_1566/" className='text-green-400'>Onur T.</a></span>
           </div>
         }
